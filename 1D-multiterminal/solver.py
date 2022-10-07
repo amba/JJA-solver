9138b178a6a746ff17079f810768c82d0d53d559
Simon Reinhardt <simon.reinhardt@physik.uni-r.de>
2022-10-07 22:57:05 +0200

    1d

--- 1D-multiterminal/solver.py
+++ 1D-multiterminal/solver.py
@@ -1,34 +1,44 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import numpy.random
 import matplotlib.pyplot as plt
+import time
+from numba import njit
 
-
+width = 1
+N_intervals=30
+delta_tol = 1e-2
+Ic_upper = 1
+phi0_upper =  0#np.pi
+phi0_lower = 0# 0.3 * np.pi# phi0_upper * np.cos(30 *np.pi / 180)
 
 # γ = phi_l - phi_r + k * x
 # I = integral_x cpr(γ) = integral_x cpr(phi_l - phi_r + k*x)
 # with x = 0 ... width
-
+@njit()
 def cpr(gamma):
     return np.sin(gamma)
 
-def current(phi_l, phi_r, width=1, k=0, N_intervals=100):
+@njit()
+def current(phi_l, phi_r, k, phi0):
     points = np.linspace(0, k * width, N_intervals)
-    rv = np.sum(cpr(phi_r - phi_l + points))
+    rv = np.sum(cpr(phi_r - phi_l -phi0 + points))
     return rv/N_intervals
 
 
 # phi1    |       phi2         |           phi4
 #       phi3            phi3 + 2pi Φ/Φ0
 
-
-def network_current(phases, k, *args, **kwargs):
+@njit()
+def network_current(phases, k):
     phi1 = phases[0]
     phi2 = phases[1]
     phi3 = phases[2]
-    return current(phi1, phi2, k=k, *args, **kwargs) + \
-        current(phi1, phi3, k=-k, *args, **kwargs)
+    return Ic_upper * current(phi1, phi2, k, phi0_upper) + \
+        current(phi1, phi3, -k, phi0_lower)
 
+@njit()
 def optimization_step(phases, k, flux):
     # phase vector = [phi1, phi2, phi3, phi4]
     # flux = Φ/Φ_0
@@ -43,17 +53,17 @@ def optimization_step(phases, k, flux):
     
     # get derivative of free energy with respect to phi2
     dF2 = 0
-    dF2 += current(phi1, phi2, k=k)
-    dF2 += current(phi3, phi2, k=-k)
-    dF2 += -current(phi2, phi_3B, k=-k)
-    dF2 += -current(phi2, phi4, k=k)
+    dF2 += Ic_upper * current(phi1, phi2, k, phi0_upper)
+    dF2 += current(phi3, phi2, -k, phi0_lower)
+    dF2 += -current(phi2, phi_3B, -k, phi0_lower)
+    dF2 += -Ic_upper * current(phi2, phi4, k, phi0_upper)
 
     # get derivative of free energy with respect to phi3
     dF3 = 0
-    dF3 += current(phi1, phi3, k=-k)
-    dF3 += -current(phi3, phi2, k=-k)
-    dF3 += current(phi2, phi_3B, k=-k)
-    dF3 += -current(phi_3B, phi4, k=-k)
+    dF3 += current(phi1, phi3, -k, phi0_lower)
+    dF3 += -current(phi3, phi2, -k, phi0_lower)
+    dF3 += current(phi2, phi_3B, -k, phi0_lower)
+    dF3 += -current(phi_3B, phi4, -k, phi0_lower)
 
 
     # do gradient descent of free energy
@@ -67,28 +77,40 @@ def optimization_step(phases, k, flux):
     # use dF to check for convergence
     return np.abs(dF2) + np.abs(dF3)
 
-k_vals = np.linspace(0, 3*np.pi, 400)
-I_vals = []
+@njit()
+def optimize_phases(phi, k, flux):
+    for i in range(2000):
+        delta = optimization_step(phi, k, flux)
+        if delta < delta_tol:
+            return 0
+    print("did not converge for k = ", k)
+    
+k_vals = np.linspace(-6*np.pi, 6*np.pi, 1000)
+Iplus_vals = []
+Iminus_vals = []
+t0 = time.time()
 for k in k_vals:
     print("k = ", k)
-    flux = 3*k
-    phi_vals = np.linspace(-np.pi, np.pi, 100)
+    flux = 2* k / (2*np.pi)
+    phi_vals = np.linspace(0, 2*np.pi, 50)
     Iphi_vals = []
-
+    phi2 = 2 * np.pi * numpy.random.rand()
+    phi3 = 2 * np.pi * numpy.random.rand()
+    phi = np.array([0, phi2, phi3, 0])
+    
     for phi4 in phi_vals:
-        phi = np.array([0, phi4/2, phi4/2, phi4])
-        for i in range(1000):
-            delta = optimization_step(phi, k, flux)
-#            print("delta = ", delta)
- #           print(phi_0)
-            if delta < 1e-3:
-                print("i = ", i)
-                break
+        phi[3] = phi4
+        optimize_phases(phi, k, flux)
         I = network_current(phi, k)
         Iphi_vals.append(I)
-    I_vals.append(np.amax(Iphi_vals))
+    Iplus_vals.append(np.amax(Iphi_vals))
+    Iminus_vals.append(np.amin(Iphi_vals))
 
-plt.plot(k_vals, I_vals)
+print("time: ", time.time() - t0)
+    
+k_vals = k_vals / (2*np.pi)
+plt.plot(k_vals, Iplus_vals)
+plt.plot(k_vals, Iminus_vals)
 plt.show()
     
     
