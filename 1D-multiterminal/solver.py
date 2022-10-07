977479aa3a52910d569500d38b8c1bbdc2d11cf9
Simon Reinhardt <simon.reinhardt@physik.uni-r.de>
2022-10-07 15:23:23 +0200

    multiterminal solver

---
+++ 1D-multiterminal/solver.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+# γ = phi_l - phi_r + k * x
+# I = integral_x cpr(γ) = integral_x cpr(phi_l - phi_r + k*x)
+# with x = 0 ... width
+
+def cpr(gamma):
+    return np.sin(gamma)
+
+def current(phi_l, phi_r, width=1, k=0, N_intervals=100):
+    points = np.linspace(0, k * width, N_intervals)
+    rv = np.sum(cpr(phi_r - phi_l + points))
+    return rv/N_intervals
+
+
+# phi1    |       phi2         |           phi4
+#       phi3            phi3 + 2pi Φ/Φ0
+
+
+def network_current(phases, k, *args, **kwargs):
+    phi1 = phases[0]
+    phi2 = phases[1]
+    phi3 = phases[2]
+    return current(phi1, phi2, k=k, *args, **kwargs) + \
+        current(phi1, phi3, k=-k, *args, **kwargs)
+
+def optimization_step(phases, k, flux):
+    # phase vector = [phi1, phi2, phi3, phi4]
+    # flux = Φ/Φ_0
+    # phi1 and phi4 are fixed
+    # optimize phi3 and phi4
+    phi1 = phases[0]
+    phi2 = phases[1] # phi2A = phi2B
+    phi3 = phases[2] #
+    phi4 = phases[3]
+
+    phi_3B = phi3 + 2*np.pi * flux
+    
+    # get derivative of free energy with respect to phi2
+    dF2 = 0
+    dF2 += current(phi1, phi2, k=k)
+    dF2 += current(phi3, phi2, k=-k)
+    dF2 += -current(phi2, phi_3B, k=-k)
+    dF2 += -current(phi2, phi4, k=k)
+
+    # get derivative of free energy with respect to phi3
+    dF3 = 0
+    dF3 += current(phi1, phi3, k=-k)
+    dF3 += -current(phi3, phi2, k=-k)
+    dF3 += current(phi2, phi_3B, k=-k)
+    dF3 += -current(phi_3B, phi4, k=-k)
+
+
+    # do gradient descent of free energy
+    epsilon = 0.1
+    phi2_new = phi2 - epsilon * dF2
+    phi3_new = phi3 - epsilon * dF3
+
+    phases[1] = phi2_new
+    phases[2] = phi3_new
+
+    # use dF to check for convergence
+    return np.abs(dF2) + np.abs(dF3)
+
+k_vals = np.linspace(0, 1*np.pi, 200)
+I_vals = []
+for k in k_vals:
+    print("k = ", k)
+    flux = 4*k
+    phi_vals = np.linspace(0, 2*np.pi, 30)
+    Iphi_vals = []
+    for phi in phi_vals:
+        phi_0 = np.array([0, 0, 0, phi])
+        
+        for i in range(100):
+            delta = optimization_step(phi_0, k, flux)
+#            print("delta = ", delta)
+ #           print(phi_0)
+            if delta < 1e-3:
+                break
+        I = network_current(phi_0, k)
+        Iphi_vals.append(I)
+    I_vals.append(np.amax(Iphi_vals))
+
+plt.plot(k_vals, I_vals)
+plt.show()
+    
+    
+    
+    
+    
+    
+    
+
+# phi_vals = np.linspace(0, 2*np.pi, 100)
+# k_vals = np.linspace(0, 30, 1000)
+# Ic_vals = []
+# for k in k_vals:
+#     Iphi = []
+#     for phi in phi_vals:
+#         I = current(0, phi, k=k)
+#         Iphi.append(I)
+#     Ic_vals.append(np.amax(Iphi))
+
+
+
+
+
+
+
+# plt.plot(k_vals, Ic_vals)
+# plt.show()
+
+
